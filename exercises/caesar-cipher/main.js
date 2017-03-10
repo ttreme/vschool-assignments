5615ab9cc17b0e4124ce90dabc79d6050a905e76
--global <--global>
2017-03-10 09:44:14 -0700

    worked on my rpg and got it functioning

---
+++ exercises/caesar-cipher/main.js
@@ -0,0 +1,83 @@
+var readlinesync = require("readline-sync");
+var input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
+var shift = parseInt(readline.question('How many letters would you like to shift? '));
+switch (expression) {
+case a:
+    letter = "b"
+    break;
+case b:
+    letter = "c"
+    break;
+case c:
+    letter = "d"
+    break;
+case d:
+    letter = "e"
+    break;
+case e:
+    letter = "b"
+    break;
+case f:
+    letter = "c"
+    break;
+case g:
+    letter = "b"
+    break;
+case h:
+    letter = "c"
+    break;
+case i:
+    letter = "b"
+    break;
+case j:
+    letter = "c"
+    break;
+case k:
+    letter = "c"
+    break;
+case l:
+    letter = "b"
+    break;
+case m:
+    letter = "c"
+    break;
+case n:
+    letter = "b"
+    break;
+case o:
+    letter = "c"
+    break;
+case p:
+    letter = "c"
+    break;
+case q:
+    letter = "c"
+    break;
+case r:
+    letter = "b"
+    break;
+case s:
+    letter = "c"
+    break;
+case t:
+    letter = "b"
+    break;
+case u:
+    letter = "c"
+    break;
+case v:
+    letter = "c"
+    break;
+case w:
+    letter = "c"
+    break;
+case x:
+    letter = "c"
+    break;
+case y:
+    letter = "b"
+    break;
+case z:
+    letter = "a"
+    break;
+}
