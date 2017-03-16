01d373d4480e6861df9a0c4ff55b8af297f79a5a
Titus Treme <tremetitus@gmail.com>
2017-03-16 12:53:08 -0600

    updated a couple of files

---
+++ exercises/speedclicker/main.js
@@ -0,0 +1,16 @@
+var num = 0;
+if (localStorage.getItem("click")) {
+    num = localStorage.getItem("click");
+    $(document).on("click", function () {
+        num++;
+        $("#printsHere").html(num);
+        localStorage.setItem("click", num);
+    })
+}
+else {
+    $(document).on("click", function () {
+        num++;
+        $("#printsHere").html(num);
+        localStorage.setItem("click", num);
+    })
+}
